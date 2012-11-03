0358a76d74be32655d6d395cf81763427e068560
Karthik Ram <karthik.ram@gmail.com>
2012-11-03 14:21:26 -0700

    foo

--- R/data_retrieval.R
+++ R/data_retrieval.R
@@ -1,8 +1,15 @@
-library(rplos)
-library(ggplot2)
-# alm title
-title <- almtitle(doi='10.1371/journal.pbio.0000012')
 
-# [1] "almdatepub"      "almdateupdated"  "almplosallviews"
-# [4] "almplotallviews" "almpubmedcentid" "almpubmedid"    
-# [7] "almtitle"        "almtotcites"     "almupdated"     
+library(ggplot2)
+library(reshape2)
+library(lubridate)
+# Data Visualizations for PLOS data
+# 
+data <- read.csv('data/many_articles.csv', header = T)
+data$total_count <- apply(sub_data[,8:ncol(sub_data)], 1, sum)
+data$publication_date <- 
+sub_data <- melt(data, id.vars = 1:7)
+
+f_data <- subset(sub_data, variable == "total_count")
+ggplot(f_data, aes(doi, value, color = journal, show_guides = FALSE)) + geom_point(aes(size = 1.2 * value), show_guide = FALSE) 
+
+
