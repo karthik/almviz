0c5bade6fa4772be8a19308f102cb490671a3610
Karthik Ram <karthik.ram@gmail.com>
2012-11-03 12:55:22 -0700

    moved out R files to a subfolder

---
+++ R/data_retrieval.R
@@ -0,0 +1,8 @@
+library(rplos)
+library(ggplot2)
+# alm title
+title <- almtitle(doi='10.1371/journal.pbio.0000012')
+
+# [1] "almdatepub"      "almdateupdated"  "almplosallviews"
+# [4] "almplotallviews" "almpubmedcentid" "almpubmedid"    
+# [7] "almtitle"        "almtotcites"     "almupdated"     
