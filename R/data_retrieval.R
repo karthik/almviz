ced510f664937ad7de075c41eabf35a8629c1a63
Karthik Ram <karthik.ram@gmail.com>
2012-11-03 15:47:07 -0700

    reorganizing a bunch of shit

--- R/data_retrieval.R
+++ R/data_retrieval.R
@@ -2,14 +2,27 @@
 library(ggplot2)
 library(reshape2)
 library(lubridate)
+require(scales)
+
 # Data Visualizations for PLOS data
 # 
 data <- read.csv('data/many_articles.csv', header = T)
-data$total_count <- apply(sub_data[,8:ncol(sub_data)], 1, sum)
-data$publication_date <- 
-sub_data <- melt(data, id.vars = 1:7)
-
-f_data <- subset(sub_data, variable == "total_count")
-ggplot(f_data, aes(doi, value, color = journal, show_guides = FALSE)) + geom_point(aes(size = 1.2 * value), show_guide = FALSE) 
+# Convert pub date to Date and get months since pub to now.
+data$publication_date <- as.Date(data$publication_date)
+data$months <- round(difftime(as.Date(now()), sub_data$publication_date, unit ="weeks")/4)
+data <- arrange(data, publication_date)
 
 
+
+
+# f_data <- subset(sub_data, variable == "counter_html")
+# ggplot(f_data, aes(doi, value, color = journal)) + geom_point(aes(size = 1.2 * value), show_guide = FALSE) 
+
+
+# ggplot(subset(sub_data, journal == "PLoS Biology"), aes(publication_date, counter_html, color = journal)) + geom_point(aes(size = 5 * scopus), show_guide = FALSE) 
+
+# ggplot(sub_data, aes(publication_date, counter_html, color = journal)) + geom_point(aes(size = 5 * scopus)) 
+
+# # + scale_y_log10()
+
+ggplot(sub_data, ) + facet_wrap(~journal)
